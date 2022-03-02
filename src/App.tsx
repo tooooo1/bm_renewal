d1bc815b72338227f99106d339a651f68b86a7a7
tooooo1 <wer4272@nate.com>
2022-03-03 01:09:43 +0900

    create : 초기 설정 세팅 ( 패키지 설치 및 기본 구조 잡기 )

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import { Home } from "./pages";
+import reset from "styled-reset";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          초기세팅
-        </p>
-      </header>
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    outline: none;
+    box-sizing: border-box;
+  }
+`;
+
+const App = () => (
+  <BrowserRouter>
+    <GlobalStyle />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import * as Styled from './styled';
+
+type Props = {};
+
+const Home = (props: Props) => {
+  
+    return (
+        <Styled.Wrapper>
+            gd
+        </Styled.Wrapper>
+  );
+}
+
+export default Home;
