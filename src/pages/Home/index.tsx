564c2480ad943ce01b5f8764f83073b09423f5d9
tooooo1 <wer4272@nate.com>
2022-03-04 22:10:10 +0900

    update : 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,46 @@
+import { useState, useEffect } from 'react';
 import * as Styled from './styled';
+// import axios from 'axios';
 
 type Props = {};
 
 const Home = (props: Props) => {
-  
+    // const [data, setData] = useState({ items: [] });
+    // useEffect(() => {
+    // const fetchData = async () => {
+    //     const result = await axios(
+    //         'https://us-central1-react-baemin.cloudfunctions.net/merchantInfo'
+    //     );
+    //     setData(result.data);
+    //     };
+    //     fetchData();
+    // }, []);
+
+    const [choiceMenu, setChoiceMenu] = useState<string>('분식');
+    const [activeIndex, setActiveIndex] = useState<number>(0);
+
+    const tabClickHandler = (index:number) => {
+        setActiveIndex(index);
+    };
+
+    
+
     return (
         <Styled.Wrapper>
-            gd
+            <Styled.Title> { choiceMenu }</Styled.Title>
+            <Styled.Menu> </Styled.Menu>
+
+
+
+
+
+
+
+
+            {/* {data.items.map((item) => (
+                <li key={item}>
+            </li>
+            ))} */}
         </Styled.Wrapper>
   );
 }
