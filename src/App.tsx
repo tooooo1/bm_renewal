e4bae1be0a312b3d96c8ebaa6914ef9c6a145ed1
tooooo1 <wer4272@nate.com>
2022-05-04 17:29:41 +0900

    font : 추가 + css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    outline: none;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Pretendard-Medium', 'Apple SD Gothic Neo', 'Noto Sans KR', sans-serif;
+  }
+`;
 
 const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
