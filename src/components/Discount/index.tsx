6f59f7e29ab255cbb9073389960aec95cb127215
tooooo1 <wer4272@nate.com>
2022-05-06 17:06:42 +0900

    feat : Discount component 추가

---
+++ src/components/Discount/index.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  middleTitle: string;
+  title: string;
+  substr1: string;
+  substr2: string;
+}
+
+const Discount = ({ middleTitle, title, substr1, substr2 }: Props) => {
+  return (
+    <div className="bg(#eee) m(1rem) p(2rem) pr(15rem)">
+      <div className="font(1.4rem)">{middleTitle}</div>
+      <div className="font(2rem) font-family('Pretendard-ExtraBold') mb(1rem)">{title}</div>
+      <div className="font-family('Pretendard-SemiBold')">{substr1}</div>
+      <div className="font-family('Pretendard-SemiBold')">{substr2}</div>
+    </div>
+  );
+};
+
+export default Discount;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,11 @@
 import Banner from '../../assets/banner.png';
+import BottomBanner from '../../assets/bottombanner.png';
 import sushi from '../../assets/menu/sushi.png';
 import jajangmyeon from '../../assets/menu/jajangmyeon.png';
 import chicken from '../../assets/menu/chicken.png';
 import rice from '../../assets/menu/rice.png';
 import cupcake from '../../assets/menu/cupcake.png';
-import { MenuItem } from '../../components';
+import { MenuItem, Discount } from '../../components';
 
 const menuList = [
   { title: '돈까스,회,일식', img: sushi },
@@ -21,13 +22,41 @@ interface Props {
 
 const Home = () => {
   return (
-    <div>
+    <div className="vbox pack">
       <img src={Banner} alt="banner" />
-      <div className="hbox m(auto) w(70%) p(4rem) space-evenly">
+      <div className="hbox m(auto) w(70%) p(4rem) mb(10vh) bb(1px) bc(#ddd) space-evenly">
         {menuList.map(({ title, img }: Props) => (
           <MenuItem key={title} img={img} title={title} />
         ))}
       </div>
+      <div className="pack font(2rem) mb(6vh) font-family('Pretendard-ExtraBold')">오늘의 할인</div>
+      <div className="hbox pack flex-wrap w(70%) mb(6vh)">
+        <Discount
+          middleTitle="육즙팡팡! 오븐마루 치킨"
+          title="최대 6천원 할인"
+          substr1="국내산 100% 냉장육만 고집합니다"
+          substr2="순살은 다리살로 부드러움 끝판왕"
+        />
+        <Discount
+          middleTitle="육즙팡팡! 오븐마루 치킨"
+          title="최대 6천원 할인"
+          substr1="국내산 100% 냉장육만 고집합니다"
+          substr2="순살은 다리살로 부드러움 끝판왕"
+        />
+        <Discount
+          middleTitle="육즙팡팡! 오븐마루 치킨"
+          title="최대 6천원 할인"
+          substr1="국내산 100% 냉장육만 고집합니다"
+          substr2="순살은 다리살로 부드러움 끝판왕"
+        />
+        <Discount
+          middleTitle="육즙팡팡! 오븐마루 치킨"
+          title="최대 6천원 할인"
+          substr1="국내산 100% 냉장육만 고집합니다"
+          substr2="순살은 다리살로 부드러움 끝판왕"
+        />
+      </div>
+      <img src={BottomBanner} alt="bottombanner" />
     </div>
   );
 };
