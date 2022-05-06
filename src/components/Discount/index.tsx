4e8018d89462f5ba0cf7bf37128995d9c61ddafc
tooooo1 <wer4272@nate.com>
2022-05-06 17:58:27 +0900

    styled : pointer 추가

--- src/components/Discount/index.tsx
+++ src/components/Discount/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Discount = ({ middleTitle, title, substr1, substr2 }: Props) => {
   return (
-    <div className="bg(#eee) m(1rem) p(2rem) pr(15rem)">
+    <div className="bg(#eee) w(550px) m(1rem) p(2rem) pr(15rem) pointer">
       <div className="font(1.4rem)">{middleTitle}</div>
       <div className="font(2rem) font-family('Pretendard-ExtraBold') mb(1rem)">{title}</div>
       <div className="font-family('Pretendard-SemiBold')">{substr1}</div>
