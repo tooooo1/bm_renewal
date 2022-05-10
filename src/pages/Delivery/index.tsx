1e59d26cfd887329167152b29d4f46d9f543e085
tooooo1 <wer4272@nate.com>
2022-05-10 11:50:02 +0900

    feat : 배달 페이지 추가

--- src/pages/Delivery/index.tsx
+++ src/pages/Delivery/index.tsx
@@ -1,8 +1,25 @@
+import ReactPlayer from 'react-player';
+import { useEffect, useState } from 'react';
+
 const Delivery = () => {
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => setCount(count + 1), 1000);
+    return () => clearInterval(timer);
+  });
   return (
     <div className="vbox pack">
-      <div className="mt(10rem) font-family(BMHANNAPro) font(7rem)">텅</div>
-      <div className="mt(1rem) font-family(BMHANNAPro)">배달</div>
+      <div className="w(100%) h(950) pointer-events(none) bg(url(https://www.coupangeats.com/wp-content/uploads/2021/09/Screen-Shot-2021-09-14-at-8.50.38-PM.png))">
+        <ReactPlayer
+          url="https://www.youtube-nocookie.com/embed/hfWZ1DlLnqU?controls=0&amp;rel=0&amp;playsinline=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.coupangeats.com&amp;widgetid=1"
+          width="100%"
+          height="100%"
+          playing={true}
+          muted={true}
+          loop={true}
+        />
+      </div>
     </div>
   );
 };
