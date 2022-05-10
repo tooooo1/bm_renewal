8a3b32ff62873db1643bea5bf9c862f2c56fa3b1
tooooo1 <wer4272@nate.com>
2022-05-10 12:12:23 +0900

    styled : name fix

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -5,14 +5,17 @@ interface Props {
   img: string;
 }
 
-const Header = ({ title, img }: Props) => {
+const MenuItem = ({ title, img }: Props) => {
   let navigate = useNavigate();
   return (
-    <div className="vbox pack p(1rem) pt(1rem) r(16) hover:cursor(pointer)+bg(#eee)+c(#2ac1bc) transition(.3s)" onClick={()=>navigate('/food')}>
+    <div
+      className="vbox pack p(1rem) pt(1rem) r(16) hover:cursor(pointer)+bg(#eee)+c(#2ac1bc) transition(.3s)"
+      onClick={() => navigate('/food')}
+    >
       <img src={img} width={100} alt="menu" />
       <div className="pt(1rem) font-family(BMHANNAPro) font(1.5rem)">{title}</div>
     </div>
   );
 };
 
-export default Header;
+export default MenuItem;
