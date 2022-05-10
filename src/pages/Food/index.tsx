2c528fe3367f82bb404194ef937fb5a626f0272c
tooooo1 <wer4272@nate.com>
2022-05-10 11:49:52 +0900

    feat : Food 페이지 추가

---
+++ src/pages/Food/index.tsx
@@ -0,0 +1,13 @@
+import sushi from '../../assets/menu/sushi.png';
+
+const Food = () => {
+  return (
+    <div>
+      <div className="vbox w(70%) m(auto) pack p(100/0)">
+        <img src={sushi} width={200} alt="menu" />
+      </div>
+    </div>
+  );
+};
+
+export default Food;
