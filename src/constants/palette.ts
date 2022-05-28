c9c6b913f9bcb1a6d336da7ef56335e4841926d1
tooooo1 <wer4272@nate.com>
2022-05-28 14:27:57 +0900

    Feat: PALETTE 추가

---
+++ src/constants/palette.ts
@@ -0,0 +1,17 @@
+const PALETTE = {
+  WHITE: '#ffffff',
+  BLACK: '#1a1a1a',
+
+  DARK_040: '#262626',
+  DARK_030: '#6C6C6C',
+  DARK_020: '#929292',
+  DARK_010: '#B2B2B2',
+  LIGHT_040: '#D2D2D2',
+  LIGHT_030: '#E2E2E2',
+  LIGHT_020: '#F2F2F2',
+  LIGHT_010: '#F8F8F8',
+
+  PRI_MAIN: '#2ac1bc',
+} as const;
+
+export default PALETTE;
