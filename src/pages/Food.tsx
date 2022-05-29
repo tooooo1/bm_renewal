7baa8de0c97544cfe9bf4c117a6350f5764650cf
tooooo1 <wer4272@nate.com>
2022-05-29 23:40:03 +0900

    Feat: 상세페이지 추가

--- src/pages/Food.tsx
+++ src/pages/Food.tsx
@@ -1,10 +1,31 @@
 import sushi from '../assets/menu/sushi.png';
+import TabBar from '../components/TabBar';
+import { IMenu, TabContents } from '../types';
+
+const menu: IMenu[] = [
+  { name: '공지사항', option: 'announcement' },
+  { name: 'FAQ', option: 'faq' },
+  { name: '1:1 문의', option: 'inquiry' },
+];
+
+const checkList: TabContents = {
+  0: <div>하1</div>,
+  1: <div>하2</div>,
+  2: <div>하3</div>,
+};
 
 const Food = () => {
   return (
     <div>
       <div className="vbox w(70%) m(auto) pack p(100/0)">
-        <img src={sushi} width={200} alt="menu" />
+        <div className="hbox">
+          <img src={sushi} width={200} alt="menu" />
+          <div>
+            <div>행복한찜닭</div>
+            <div>여기는 메뉴</div>
+          </div>
+        </div>
+        <TabBar menu={menu} checkList={checkList} />
       </div>
     </div>
   );
