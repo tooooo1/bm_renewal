5831804185ae69115967b4ab3e7c8633861b0345
tooooo1 <wer4272@nate.com>
2022-06-13 14:23:31 +0900

    Style: props 제거

--- src/components/Goods.tsx
+++ src/components/Goods.tsx
@@ -1,16 +1,16 @@
 import { GoodsProps } from '../types';
 import sushi from '../assets/menu/sushi.png';
 
-const Goods = (props: GoodsProps) => {
+const Goods = ({ name, price, onClick }: GoodsProps) => {
   return (
     <div
       className="vpack w(25%) p(2rem/0) r(16) hover:cursor(pointer)+bg(#eee)+c(#2ac1bc) transition(.3s)"
-      onClick={props.onClick}
+      onClick={onClick}
     >
-      {/* <img src={props.imgSrc} /> */}
+      {/* <img src={imgSrc} /> */}
       <img src={sushi} width="50%" />
-      <div>{props.name}</div>
-      <div>{props.price}원</div>
+      <div>{name}</div>
+      <div>{price}원</div>
     </div>
   );
 };
